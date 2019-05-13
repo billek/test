3d619db6830a1c22c1393b69815eb337bd42a1ba
Ivan <example@gmail.com>
2019-05-13 16:59:11 +0400

    Это наш коммит

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// let a = 5;
+// console.log(a);
+
+// let isChecked = false,
+// isClose = false;
+
+// console.log(isChecked || !isClose);
+
+console.log(typeof(null));
+
+console.log('arr' + '- object');
+console.log(4 + '- object');
